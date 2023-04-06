93ab18ec847dd9f2158c36e7596f6b27bb29e02d
Mark Heckler <mark.heckler@gmail.com>
2023-04-05 20:13:58 -0400

    Initial commit of SCF native binary + OCI cloud native buildpack image

--- src/main/java/com/thehecklers/scfafaircraftfn/FunctionComponent.java
+++ src/main/java/com/thehecklers/scfafaircraftfn/FunctionComponent.java
@@ -1,30 +1,30 @@
 package com.thehecklers.scfafaircraftfn;
 
-import com.microsoft.azure.functions.*;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
-import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
-import org.springframework.stereotype.Component;
+//import com.microsoft.azure.functions.*;
+//import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+//import com.microsoft.azure.functions.annotation.FunctionName;
+//import com.microsoft.azure.functions.annotation.HttpTrigger;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Optional;
+//import java.util.function.Function;
 
-import java.util.Optional;
-import java.util.function.Function;
-
-@Component
+//@Component
 public class FunctionComponent {
-    private Function<Aircraft, Aircraft> ac;
-
-    public FunctionComponent(Function<Aircraft, Aircraft> ac) {
-        this.ac = ac;
-    }
-
-    @FunctionName("ac")
-    public HttpResponseMessage acFunction(@HttpTrigger(name = "req",
-            methods = {HttpMethod.GET, HttpMethod.POST},
-            authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<Aircraft>> request,
-                                          ExecutionContext context) {
-        return request.createResponseBuilder(HttpStatus.OK)
-                .header("Content-Type", "application/json")
-                .body(ac.apply(request.getBody().get()))
-                .build();
-    }
+//    private Function<Aircraft, Aircraft> ac;
+//
+//    public FunctionComponent(Function<Aircraft, Aircraft> ac) {
+//        this.ac = ac;
+//    }
+//
+//    @FunctionName("ac")
+//    public HttpResponseMessage acFunction(@HttpTrigger(name = "req",
+//            methods = {HttpMethod.GET, HttpMethod.POST},
+//            authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<Aircraft>> request,
+//                                          ExecutionContext context) {
+//        return request.createResponseBuilder(HttpStatus.OK)
+//                .header("Content-Type", "application/json")
+//                .body(ac.apply(request.getBody().get()))
+//                .build();
+//    }
 }
